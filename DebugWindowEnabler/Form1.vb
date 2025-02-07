3d497349fc53b837b0b5039d4ce335db1cb8f8fa
stopyouhelper24 <101508503+stopyouhelper24@users.noreply.github.com>
2025-02-07 21:56:15 +0100

    Add files via upload

---
+++ DebugWindowEnabler/Form1.vb
@@ -0,0 +1,8 @@
+﻿Imports CV
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim DebugWindow As New CV.A_Test_Thing
+        DebugWindow.Show()
+        Close()
+    End Sub
+End Class
---
+++ DebugWindowEnabler/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Debug Window Enabler")>
+<Assembly: AssemblyDescription("This dll can open a Secret Debug Window thats hides inside the Sebs SW CV code.")>
+<Assembly: AssemblyCompany("Freesoft")>
+<Assembly: AssemblyProduct("Debug Window Enabler")>
+<Assembly: AssemblyCopyright("Copyright © Freesoft 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d9d106fe-f4b8-4446-a73c-73f695ec4ab2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
